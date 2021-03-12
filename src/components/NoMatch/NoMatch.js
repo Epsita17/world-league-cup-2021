cca4e85abb8f0a9ce67277e05b1d6c94f0c99f02
Epsita17 <epsitaalam17@gmail.com>
2021-03-13 04:22:06 +0600

    World League Cup 2021 Assignment 8 submit as public Repo due Netlify failed

---
+++ src/components/NoMatch/NoMatch.js
@@ -0,0 +1,16 @@
+// / import React from 'react';
+import React from "react";
+
+import "./NoMatch.css";
+
+
+const NoMatch = () => {
+  return (
+    <div>
+      <h1 className="notFound">404 Page Not Found</h1>
+      
+    </div>
+  );
+};
+
+export default NoMatch;
